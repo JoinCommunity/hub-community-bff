ec51e7c531ff2cc2678f6986225b63c2cc39c607
Pedro Goiania <pedro.paulods@hotmail.com>
2025-09-18 12:34:32 -0300

    fixed linter

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,10 +1,11 @@
 module.exports = {
-  printWidth: 100,
+  printWidth: 80,
   tabWidth: 2,
   semi: true,
   singleQuote: true,
   trailingComma: 'es5',
   bracketSpacing: true,
+  bracketSameLine: true,
   arrowParens: 'always',
   endOfLine: 'lf',
 };
